87f17222e7a681d159a36bdf8db5eec151e35ea6
ZoiliSanchez <38706142+ZoilaSanchez@users.noreply.github.com>
2022-06-24 11:28:55 -0600

    validaciones

--- src/controllers/imageControllers.js
+++ src/controllers/imageControllers.js
@@ -90,6 +90,11 @@ usarCtrl.insertarImagenContenido=async(req,res)=>{
 
 usarCtrl.obtenerImagen = async (req, res) => {
     const { id } = req.params;
+    if(id === undefined){
+        const error = new Error("Imagen no existe");
+        return res.status(400).json({ msg: error.message });
+    
+    }
     const imagenEncontrada = await Imagen.findById(id);
     if (!imagenEncontrada ) {
         const error = new Error("Imagen no existe");
